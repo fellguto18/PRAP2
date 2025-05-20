845aba8356cb89f5d584f9a7a78e3c3358b6ae74
fellguto18 <105678066+fellguto18@users.noreply.github.com>
2025-05-20 07:49:05 -0300

    Update maior_distancia.py

--- AP2/maior_distancia/maior_distancia.py
+++ AP2/maior_distancia/maior_distancia.py
@@ -2,17 +2,16 @@ def maior_distancia(grafo):
     def bfs(inicio):
         visitado = []
         for i in range(len(grafo)):
-            visitado.append(0)  # Inicializa todos os vértices como não visitados
+            visitado.append(0)  
         
         distancia = []
         for i in range(len(grafo)):
-            distancia.append(-1)  # Inicializa todas as distâncias como -1
+            distancia.append(-1) 
         
         fila = []
-        fila.append(inicio)  # Adiciona o vértice inicial à fila
-        visitado[inicio] = 1  # Marca o vértice inicial como visitado
-        distancia[inicio] = 0  # A distância do vértice inicial para ele mesmo é 0
-
+        fila.append(inicio)  
+        visitado[inicio] = 1  
+        distancia[inicio] = 0 
         while len(fila) > 0:
             atual = fila[0]
             nova_fila = []
@@ -21,16 +20,16 @@ def maior_distancia(grafo):
             fila = nova_fila
 
             for i in range(len(grafo[atual])):
-                if grafo[atual][i] != 0 and visitado[i] == 0:  # Verifica se há uma aresta e se o vértice não foi visitado
-                    visitado[i] = 1  # Marca o vértice como visitado
-                    distancia[i] = distancia[atual] + 1  # Atualiza a distância
-                    fila.append(i)  # Adiciona o vértice à fila
+                if grafo[atual][i] != 0 and visitado[i] == 0:  
+                    visitado[i] = 1  
+                    distancia[i] = distancia[atual] + 1  
+                    fila.append(i)  
         
         return distancia
 
     max_distancia = 0
     for vertice in range(len(grafo)):
-        distancias = bfs(vertice)  # Realiza a BFS a partir de cada vértice
+        distancias = bfs(vertice)  
         for i in range(len(distancias)):
             if distancias[i] > max_distancia:
                 max_distancia = distancias[i]
